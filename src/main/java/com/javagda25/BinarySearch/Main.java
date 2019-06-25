2538836f4cf4dbc3024d4a0ec39a7e365fa10783
Agata <agata.weltrowska@outlook.com>
2019-06-25 19:27:25 +0200

    binary search done

--- src/main/java/com/javagda25/BinarySearch/Main.java
+++ src/main/java/com/javagda25/BinarySearch/Main.java
@@ -4,6 +4,29 @@ package com.javagda25.BinarySearch;
 
 public class Main {
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 6, 8, 9};
+        System.out.println(binarySearch(array, 2));
+    }
 
+    public static int binarySearch(int[] array, int searchValue) {
+        int indexLeftBound = 0;
+        int indexRightBound = array.length - 1;
+        int index = (indexRightBound + indexLeftBound) / 2;
+        int value = array[index];
+
+        while (indexLeftBound <= indexRightBound) {
+            if (value == searchValue) {
+                return index;
+            } else if (value < searchValue) {
+                indexLeftBound = index + 1;
+            } else if (value > searchValue) {
+                indexRightBound = index - 1;
+            }
+
+            index = (indexRightBound + indexLeftBound) / 2;
+            value = array[index];
+
+        }
+        return -1;
     }
 }
