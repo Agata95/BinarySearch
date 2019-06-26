fb542244388c92b45f1964237b1ee0d9070c1b87
Agata <agata.weltrowska@outlook.com>
2019-06-26 14:06:06 +0200

    binary search recursive done

--- src/main/java/com/javagda25/BinarySearch/Main.java
+++ src/main/java/com/javagda25/BinarySearch/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 6, 8, 9};
         System.out.println(binarySearch(array, 2));
+        System.out.println(binarySearchRecursive(array, 6, 0, array.length - 1));
     }
 
     public static int binarySearch(int[] array, int searchValue) {
@@ -19,13 +20,28 @@ public class Main {
                 return index;
             } else if (value < searchValue) {
                 indexLeftBound = index + 1;
-            } else if (value > searchValue) {
+            } else {
                 indexRightBound = index - 1;
             }
 
             index = (indexRightBound + indexLeftBound) / 2;
             value = array[index];
 
+        }
+        return -1;
+    }
+
+    private static int binarySearchRecursive(int[] array, int searchValue, int indexLeftBound, int indexRightBound) {
+        if (indexLeftBound <= indexRightBound) {
+            int index = (indexRightBound + indexLeftBound) / 2;
+            int value = array[index];
+            if (value == searchValue) {
+                return index;
+            }
+            if (value > searchValue) {
+                return binarySearchRecursive(array, searchValue, indexLeftBound, index - 1);
+            }
+            return binarySearchRecursive(array, searchValue, index + 1, indexRightBound);
         }
         return -1;
     }
